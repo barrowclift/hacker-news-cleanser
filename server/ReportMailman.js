2edcab902865cef784769a593a181f81daf5c7e7
Marc Barrowclift <marc@barrowclift.me>
2024-10-27 11:46:10 -0400

    Fixing broken image link in Hacker News email report

--- server/ReportMailman.js
+++ server/ReportMailman.js
@@ -490,7 +490,7 @@ export default class ReportMailman {
                         <td bgcolor="#ff6600">
                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="padding:2px">
                                 <tr>
-                                    <td style="width:18px;padding-right:4px"><a href="https://news.ycombinator.com"><img src="https://news.ycombinator.com/y18.gif" width="18" height="18" style="border:1px white solid;"></a></td>
+                                    <td style="width:18px;padding-right:4px"><a href="https://news.ycombinator.com"><img src="https://news.ycombinator.com/y18.svg" width="18" height="18" style="border:1px white solid;"></a></td>
                                     <td style="line-height:12pt; height:10px;"><span class="pagetop"><b class="hnname"><a href="https://news.ycombinator.com/news">Hacker News</a></b></td>
                                     <td style="text-align:right;padding-right:4px;"><span class="pagetop">
         <a id="me" href="https://news.ycombinator.com/user?id=` + username + `">` + username + `</a> (` + totalStoriesCleansedSinceBeginningOfTime + ` stories cleansed, ` + cleansedStories.length + ` this week)</span></td>
